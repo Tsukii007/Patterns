4d06e5cb169a10ea681c6bf40c33a106abdf1383
Bhavyansh <tyagibhavyansh123@gmail.com>
2025-09-10 11:22:59 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
-void pattern6(int N) {
-    for (int i = 0; i < N; i++) {
-        for (int j = N; j > i; j--) {
-            printf("j ");
+
+void pattern6(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = N; j > i; j--)
+        {
+            printf("%d ", N - j + 1);
         }
         printf("\n");
     }
 }
-int main() {
-    int N;
-    printf("Enter number of rows: ");
-    scanf("%d", &N);
+int main()
+{
+    printf("Enter Number of Rows: ");
+    scanf("%d",&N);
     pattern6(N);
     return 0;
 }
